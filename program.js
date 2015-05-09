45853c26d0a1ea90a8f37e9059ad8ad7f77647d9
bumaociyuan <zx19880528@gmail.com>
2015-05-10 03:05:05 +0800

    5

--- program.js
+++ program.js
@@ -1,10 +1,14 @@
-function getShortMessages(messages) {
-  // SOLUTION GOES HERE
-  return messages.filter(function (n) {
-  	return n.message.length < 50;
-  }).map(function (n) {
-  	return n.message;
-  })
+function checkUsersValid(goodUsers)
+{
+	var temp = goodUsers.map(function(d) {
+		return d.id;
+	});
+ 	return function allUsersValid(submittedUsers) {
+  	var result = submittedUsers.every(function (b, index, array) {
+		var id = b.id;
+  		return temp.indexOf(id) != -1
+  	});
+  	return result;
+  };
 }
-
-module.exports = getShortMessages
+module.exports = checkUsersValid
